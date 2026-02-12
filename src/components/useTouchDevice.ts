68c7b303aacf2661ca6050bf81a6931e9e250ea9
Brian Beck <exogen@gmail.com>
2026-02-12 09:17:44 -0800

    fix useTouchDevice query

--- src/components/useTouchDevice.ts
+++ src/components/useTouchDevice.ts
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 
-const query = "(pointer: coarse) and (hover: none)";
+// Only check pointer: coarse. Adding "hover: none" would be more precise but
+// Samsung Android devices incorrectly report hover: hover for touchscreens.
+// See: https://www.ctrl.blog/entry/css-media-hover-samsung.html
+const query = "(pointer: coarse)";
 
 export function useTouchDevice() {
   const [isTouch, setIsTouch] = useState<boolean | null>(null);
